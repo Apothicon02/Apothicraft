25afec312785d5886123bc1e4dcd21a34352f170
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-01-12 22:51:12 -0500

    - Added compatibility with Modernfix. - Readded entity loot tables again. - Dirt blocks are now dropped when broken with a  shovel. - Packed mud can now be crafted from 1 dirt and 1 wheat. - Drowned can no longer drop copper. - Iron golems can no longer drop iron. - Zombies can no longer drop iron. - Husks can no longer drop iron. - Doubled drop rate of wither skeleton skulls. - Piglins and piglin brutes now drop porkchops. - Enderman always drop at least 1 ender pearl. - Mud bricks can now be crafted from 4 packed mud. - Mud brick stairs, slabs, and walls can now be crafted from mud bricks in a 2x2 grid. - Mud bricks and packed mud can now be efficiently destroyed by shovels OR pickaxes.

--- src/main/java/com/Apothic0n/Apothicraft/core/events/ForgeEvents.java
+++ src/main/java/com/Apothic0n/Apothicraft/core/events/ForgeEvents.java
@@ -3,6 +3,10 @@ package com.Apothic0n.Apothicraft.core.events;
 import com.Apothic0n.Apothicraft.Apothicraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.commands.RecipeCommand;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
@@ -15,9 +19,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -28,10 +34,38 @@ import static com.Apothic0n.Apothicraft.core.ApothicraftMath.getOffsetDouble;
 
 @Mod.EventBusSubscriber(modid = Apothicraft.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeEvents {
+    static List<String> whitelist = List.of(
+            "gameplay/fishing/fish", "gameplay/fishing/junk", "gameplay/fishing/treasure",
+
+            "entities/allay", "entities/armor_stand", "entities/axolotl", "entities/bat", "entities/bee", "entities/blaze", "entities/camel", "entities/cat", "entities/cave_spider", "entities/chicken", "entities/cod", "entities/cow",
+            "entities/creeper", "entities/dolphin", "entities/donkey", "entities/drowned", "entities/elder_guardian", "entities/ender_dragon", "entities/enderman", "entities/endermite", "entities/evoker", "entities/fox", "entities/frog",
+            "entities/ghast", "entities/giant", "entities/glow_squid", "entities/goat", "entities/guardian", "entities/hoglin", "entities/horse", "entities/husk", "entities/illusioner", "entities/iron_golem", "entities/llama",
+            "entities/magma_cube", "entities/mooshrom", "entities/mule", "entities/ocelot", "entities/panda", "entities/parrot", "entities/phantom", "entities/pig", "entities/piglin", "entities/piglin_brute", "entities/pillager",
+            "entities/player", "entities/polar_bear", "entities/pufferfish", "entities/rabbit", "entities/ravager", "entities/salmon", "entities/sheep", "entities/shulker", "entities/silverfish", "entities/skeleton", "entities/skeleton_horse",
+            "entities/slime", "entities/sniffer", "entities/snow_golem", "entities/spider", "entities/squid", "entities/stray", "entities/strider", "entities/tadpole", "entities/trader_llama", "entities/tropical_fish", "entities/turtle",
+            "entities/vex", "entities/villager", "entities/vindicator", "entities/wandering_trader", "entities/warden", "entities/witch", "entities/wither", "entities/wither_skeleton", "entities/wolf", "entities/zoglin", "entities/zombie",
+            "entities/zombie_horse", "entities/zombie_villager", "entities/zombified_piglin",
+
+            "entities/sheep/black", "entities/sheep/blue", "entities/sheep/brown", "entities/sheep/cyan", "entities/sheep/gray", "entities/sheep/green", "entities/sheep/light_blue", "entities/sheep/light_gray", "entities/sheep/lime",
+            "entities/sheep/magenta", "entities/sheep/orange", "entities/sheep/pink", "entities/sheep/purple", "entities/sheep/red", "entities/sheep/white", "entities/sheep/yellow",
+
+            "blocks/campfire", "blocks/torch",
+            "blocks/dirt", "blocks/coarse_dirt", "blocks/dirt_path", "blocks/grass_block", "blocks/podzol", "blocks/rooted_dirt",
+            "blocks/packed_mud", "blocks/mud_bricks", "blocks/mud_brick_wall", "blocks/mud_brick_stairs", "blocks/mud_brick_slab"
+    );
     @SubscribeEvent
     static void lootTableLoadEvent(LootTableLoadEvent event) {
-        if (!event.getName().getNamespace().equals("apothicraft")) {
+        if (!whitelist.contains(event.getName().getPath()) && (event.getName().getNamespace().equals("minecraft") || event.getName().getNamespace().equals("create"))) {
             event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    static void playerJoined(EntityJoinLevelEvent event) {
+        Entity entity = event.getEntity();
+        if (entity.getType().equals(EntityType.PLAYER) && !event.getLevel().isClientSide) {
+            ServerPlayer player = (ServerPlayer) entity;
+            player.awardRecipes(event.getLevel().getRecipeManager().getRecipes());
         }
     }
 
--- src/main/java/com/Apothic0n/Apothicraft/mixin/RecipeManagerMixin.java
+++ src/main/java/com/Apothic0n/Apothicraft/mixin/RecipeManagerMixin.java
@@ -1,85 +1,21 @@
 package com.Apothic0n.Apothicraft.mixin;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
+import com.google.common.collect.Iterators;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.util.GsonHelper;
-import net.minecraft.util.profiling.ProfilerFiller;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraftforge.common.crafting.conditions.ICondition;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 @Mixin(RecipeManager.class)
 public abstract class RecipeManagerMixin {
-
-    @Shadow private boolean hasErrors;
-
-    @Shadow @Final private static Logger LOGGER;
-
-    @Shadow @Final private ICondition.IContext context;
-
-    @Shadow
-    public static Recipe<?> fromJson(ResourceLocation p_44046_, JsonObject p_44047_, ICondition.IContext context) {
-        return null;
-    }
-
-    @Shadow private Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> recipes;
-
-    @Shadow private Map<ResourceLocation, Recipe<?>> byName;
-
-    /**
-     * @author Apothicon
-     * @reason Disable any recipes not added by Apothicraft
-     */
-    @Overwrite
-    protected void apply(Map<ResourceLocation, JsonElement> p_44037_, ResourceManager p_44038_, ProfilerFiller p_44039_) {
-        this.hasErrors = false;
-        Map<RecipeType<?>, ImmutableMap.Builder<ResourceLocation, Recipe<?>>> map = Maps.newHashMap();
-        ImmutableMap.Builder<ResourceLocation, Recipe<?>> builder = ImmutableMap.builder();
-
-        for(Map.Entry<ResourceLocation, JsonElement> entry : p_44037_.entrySet()) {
-            ResourceLocation resourcelocation = entry.getKey();
-            if (resourcelocation.getNamespace().equals("apothicraft")) {
-                if (resourcelocation.getPath().startsWith("_"))
-                    continue; //Forge: filter anything beginning with "_" as it's used for metadata.
-
-                try {
-                    if (entry.getValue().isJsonObject() && !net.minecraftforge.common.crafting.CraftingHelper.processConditions(entry.getValue().getAsJsonObject(), "conditions", this.context)) {
-                        LOGGER.debug("Skipping loading recipe {} as it's conditions were not met", resourcelocation);
-                        continue;
-                    }
-                    Recipe<?> recipe = fromJson(resourcelocation, GsonHelper.convertToJsonObject(entry.getValue(), "top element"), this.context);
-                    if (recipe == null) {
-                        LOGGER.info("Skipping loading recipe {} as it's serializer returned null", resourcelocation);
-                        continue;
-                    }
-                    map.computeIfAbsent(recipe.getType(), (p_44075_) -> {
-                        return ImmutableMap.builder();
-                    }).put(resourcelocation, recipe);
-                    builder.put(resourcelocation, recipe);
-                } catch (IllegalArgumentException | JsonParseException jsonparseexception) {
-                    LOGGER.error("Parsing error loading recipe {}", resourcelocation, jsonparseexception);
-                }
-
-            }
-        }
-
-        this.recipes = map.entrySet().stream().collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, (p_44033_) -> {
-            return p_44033_.getValue().build();
-        }));
-        this.byName = builder.build();
-        LOGGER.info("Loaded {} recipes", (int)map.size());
+    @Redirect(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE", target = "Ljava/util/Set;iterator()Ljava/util/Iterator;", ordinal = 0))
+    private Iterator<Map.Entry<ResourceLocation, JsonElement>> filterRecipes(Set<Map.Entry<ResourceLocation, JsonElement>> instance) {
+        return Iterators.filter(instance.iterator(), entry -> (!entry.getKey().getNamespace().equals("minecraft") && !entry.getKey().getNamespace().equals("create")));
     }
 }
